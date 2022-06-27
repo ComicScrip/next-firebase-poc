4af718b17b7bf2e734d47acddad948053f1cba04
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-06-27 13:44:26 +0200

    remove lc contents

--- services/firebase.js
+++ services/firebase.js
@@ -3,13 +3,7 @@ import firebase from 'firebase/app';
 
 // https://console.firebase.google.com/project/next-firebase-poc/settings/general/web:N2Q2YzAyMDYtZGIzOC00OGE3LThjYTEtYzM5Y2Q0NGI5NDIw
 const firebaseConfig = {
-  apiKey: 'AIzaSyDf9G8VZbcyJ5KSybm0VBLmLthFuvkHrcc',
-  authDomain: 'next-firebase-poc.firebaseapp.com',
-  projectId: 'next-firebase-poc',
-  storageBucket: 'next-firebase-poc.appspot.com',
-  messagingSenderId: '73142146303',
-  appId: '1:73142146303:web:4090d6f533b1802c7d68ca',
-  measurementId: 'G-RLW6DXV66D',
+  // TODO
 };
 
 if (!firebase?.apps?.length) firebase?.initializeApp(firebaseConfig);
