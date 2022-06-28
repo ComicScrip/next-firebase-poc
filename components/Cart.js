198a6ec279cfa2fbf085aae4428921f0454a02ce
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-06-29 00:09:51 +0200

    fix cart ?

--- components/Cart.js
+++ components/Cart.js
@@ -35,47 +35,47 @@ export default function Cart({
   return (
     <div
       className={`shadow-lg box-content transition-all absolute ${
-        isOpen ? 'h-[60vh]' : 'h-[80px]'
+        isOpen ? 'h-[75%]' : 'h-[80px]'
       }  bottom-0 w-full bg-gray-100 max-w-[800px] rounded-md overflow-hidden`}
     >
       <div
         className='w-full border-b text-start cursor-pointer flex justify-between px-[20px] pt-[30px] pb-[25px]'
         onClick={() => {
+          if (isOpen) {
+            bottomRef.current.scrollIntoView({ behavior: 'smooth' });
+            if (!customerName) setFocus('customerName');
+          }
           onClick();
-          bottomRef.current.scrollIntoView({ behavior: 'smooth' });
-          if (!customerName && !isOpen) setFocus('customerName');
         }}
       >
-        <div className='flex '>
+        <div className='flex'>
           <div
             className={`border-solid border-l-black border-l-[12px] border-y-transparent border-y-[12px] border-r-0 mr-4 h-4 transform-all ${
               isOpen ? 'rotate-90' : ''
             }`}
           />
-
-          <p className='font-semibold'>Your order</p>
+          {items.length !== 0 ? (
+            <>
+              {items.reduce((acc, { quantity }) => acc + quantity, 0)} items
+              {' - '}
+              <span className='font-semibold'>${totalPrice}</span>
+            </>
+          ) : (
+            <div>{'Click on "+" to order'}</div>
+          )}
         </div>
-        <div className='flex items-center relative bottom-1'>
-          <p>
-            {items.length ? (
-              <>
-                {items.reduce((acc, { quantity }) => acc + quantity, 0)} items
-                {' - '}
-                <span className='font-semibold'>${totalPrice}</span>
-              </>
-            ) : (
-              'Click on "+"'
-            )}
-          </p>
 
+        <div className='flex items-center relative bottom-1'>
           <button
-            className={`p-1 block bg-blue-500 rounded-lg text-white font-semibold px-3 ml-4`}
+            className={`${
+              !items.length ? 'opacity-70' : ''
+            } p-1 block bg-gradient-to-r from-blue-500 to-indigo-400 rounded-lg text-white font-semibold px-3 ml-4`}
           >
-            Checkout
+            {isOpen ? 'See Menu' : 'Checkout'}
           </button>
         </div>
       </div>
-      <div className='relative overflow-y-scroll h-[30vh] pb-2'>
+      <div className='relative overflow-y-scroll h-[80%] pb-2'>
         <form className='p-[20px]' onSubmit={handleSubmit(onOrder)}>
           {items.map(({ product, quantity }) => {
             const inCart = items.find(
